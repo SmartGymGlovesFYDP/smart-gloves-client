7edd3a0684dafcd7787da440371c697bf33a96a7
Shahil <35501255+spatel777@users.noreply.github.com>
2021-05-01 16:22:27 -0400

    Updated gitignore to ignore Wifi Login file

--- arduino/Development/WiFi_Login.cpp
+++ arduino/Development/WiFi_Login.cpp
@@ -3,5 +3,5 @@
 // This is the definition of your variable.  It can only happen in one place.
 // You must include Wifi_Login.h so that the compiler matches it to the correct
 // one, and doesn't implicitly convert it to static.
-const char* WIFI_SSID = "YOUR_WiFi_SSID";
-const char* WIFI_PASSWORD = "YOUR_WiFi_PASSWORD";
+const char* WIFI_SSID = "YOUR_SSID";
+const char* WIFI_PASSWORD = "YOUR_PASSWORD";
