5447c9b2ed2c0cc99e2061bbe67d873b1d341b50
Shaihl Patel <shahilpatel@hotmail.com>
2021-05-01 16:29:59 -0400

    some changes

--- arduino/Development/WiFi_Login.cpp
+++
@@ -1,7 +0,0 @@
-#include "Wifi_Login.h"
-
-// This is the definition of your variable.  It can only happen in one place.
-// You must include Wifi_Login.h so that the compiler matches it to the correct
-// one, and doesn't implicitly convert it to static.
-const char* WIFI_SSID = "YOUR_SSID";
-const char* WIFI_PASSWORD = "YOUR_PASSWORD";
