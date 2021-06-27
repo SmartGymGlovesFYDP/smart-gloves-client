0d94d91ff0d5343f9f45021ea67329efbf6bdd1c
Stefan Caloian <stefc12341@outlook.com>
2021-06-27 13:08:08 -0400

    added details to separate files

--- client/app/screens/ProfileScreen.js
+++ client/app/screens/ProfileScreen.js
@@ -13,6 +13,7 @@ import ManageGloves from "./ProfileScreens/ManageGloves";
 import Achievements from "./ProfileScreens/Achievements";
 import EditProfile from "./ProfileScreens/EditProfile";
 import Goals from "./ProfileScreens/Goals";
+import DetailsScreen from "./ProfileScreens/DetailsScreen";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -224,51 +225,51 @@ function HomeScreen({ navigation }) {
 }
 
 
-function DetailsScreen({ navigation }) {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
+// function DetailsScreen({ navigation }) {
+//   const [expoPushToken, setExpoPushToken] = useState('');
+//   const [notification, setNotification] = useState(false);
+//   const notificationListener = useRef();
+//   const responseListener = useRef();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+//   useEffect(() => {
+//     registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
 
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
+//     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+//       setNotification(notification);
+//     });
 
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
+//     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+//       console.log(response);
+//     });
 
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+//     return () => {
+//       Notifications.removeNotificationSubscription(notificationListener.current);
+//       Notifications.removeNotificationSubscription(responseListener.current);
+//     };
+//   }, []);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </View>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
-  );
-}
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'space-around',
+//       }}>
+//       <Text>Your expo push token: {expoPushToken}</Text>
+//       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Title: {notification && notification.request.content.title} </Text>
+//         <Text>Body: {notification && notification.request.content.body}</Text>
+//         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+//       </View>
+//       <Button
+//         title="Press to schedule a notification"
+//         onPress={async () => {
+//           await schedulePushNotification();
+//         }}
+//       />
+//     </View>
+//   );
+// }
 
 const Stack = createStackNavigator();
 
---
+++ client/app/screens/ProfileScreens/DetailsScreen.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useRef  } from 'react';
+import { Button, View, Image, Text, Platform, TouchableOpacity, SafeAreaView, StyleSheet, ScrollView   } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as ImagePicker from 'expo-image-picker';
+import * as Notifications from 'expo-notifications';
+import { IconButton, Colors } from 'react-native-paper';
+import * as firebase from "firebase";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+  
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+  
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
+
+  function DetailsScreen({ navigation }) {
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+  
+    useEffect(() => {
+      registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+  
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+      });
+  
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+  
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+        Notifications.removeNotificationSubscription(responseListener.current);
+      };
+    }, []);
+  
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Title: {notification && notification.request.content.title} </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        </View>
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
+      </View>
+    );
+  }
+  
+  
+export default DetailsScreen;
